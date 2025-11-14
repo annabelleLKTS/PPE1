9d61ec89ebb43d89e9ac81c0f35a9c7b16a0e381
annabelleLKTS <anniacontact@gmail.com>
2025-11-14 08:51:17 +0100

    suite_miniprojet

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -12,49 +12,59 @@ html_page="tableau.html"
 
 #on déclare le html
 
-echo "<!DOCTYPE html>"
-echo "<html>"
-echo "<head>"
-echo "	<meta charset='UTF-8' />"
-echo "	<title>Répertoire des liens de PPE1</title>"
-echo "</head>"
-echo "<body>"
-echo "<h2>Tableau liens PPE1</h2>"
-echo "<table border='1'>" >  ../tableau.html
-echo 	"<tr>"
-echo		"<th>NUMERO</th>" >> ../tableau.html
-echo            "<th>LIENS</th>" >> ../tableau.html
-echo            "<th>ENCODAGE</th>" >> ../tableau.html
-echo            "<th>NOMBRE DE MOTS</th>" >> ../tableau.html
-echo	"</tr>"
-
+echo -e "
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css" />
+<meta charset='UTF-8' />
+<title>Répertoire des liens de PPE1</title>
+</head>
+<body>
+<h2>Tableau liens PPE1</h2>
+<table border='1'>
+<tr>
+	<th> NUMERO </th>
+	<th> LIENS </th>
+	<th> CODE HTTP </th>
+	<th> ENCODAGE </th>
+	<th> NOMBRE DE MOTS </th>
+</tr>
+"
 
 while read -r line;
 do
 
-curl -i -L -s -w "%{content_type}\n%{http_code}\n" -o tmp.txt "$line" > metadata.tmp
+	data=$(curl -i -L -s -w "%{content_type}\n%{http_code}\n" -o tmp.txt "$line")
 
-content_type=$(head -1 metadata.tmp)
+         content_type=$(echo "$data" | head -n 1)
 
-charset=$(echo $content_type | grep -E -o "charset=.*" | cut -d= -f2)
+	http_code=$(echo "$data" | tail -n 1)
+
+	charset=$(echo "$content_type" | grep -E -o "charset=.*" | cut -d= -f2)
         [ -z "$charset" ] && charset="Inconnu"
 
-nb_mots=$(cat tmp.txt | lynx -dump -stdin -nolist | wc -w)
+	nb_mots=$(cat tmp.txt | lynx -dump -stdin -nolist | wc -w)
 
 
-echo    "<tr>"
-echo            "<td>${lineno}</td>"
-echo            "<td>${line}</td>"
-echo            "<td>${charset}</td>" 
-echo            "<td>${nb_mots}</td>"
-echo    "</tr>"
+	echo -e "
+	<tr>
+		<td>${lineno}</td>
+		<td>${line}</td>
+		<td>${http_code}</td>
+		<td>${charset}</td>
+		<td>${nb_mots}</td>
+	</tr>
+	"
 
 lineno=$((lineno +1))
 
-done < "$liens" >> ../tableau.html 
+done < "$liens"
 
-echo "</table>"
-echo "</body>"
-echo "</html>" >>  "$html_page"
+echo -e "
+</table>
+</body>
+</html>
+"
 
 echo "fichier html généré : $html_page"
